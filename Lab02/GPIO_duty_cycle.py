e4ddac3b70bad87d90d82ef795a2c64869a5a028
Jui-Wen, Yeh <B05208038@ntu.edu.tw>
2019-05-16 10:56:20 +0800

    GPIO test, debug

--- Lab02/GPIO_duty_cycle.py
+++ Lab02/GPIO_duty_cycle.py
@@ -2,16 +2,14 @@ import time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(16, GPIO.OUT)
-GPIO.setup(18, GPIO.OUT)
-GPIO.setup(22, GPIO.OUT)
+#GPIO.setup(18, GPIO.OUT)
+#GPIO.setup(22, GPIO.OUT)
 p1 = GPIO.PWM(16, 1)
 #p2 = GPIO.PWM(18, 1)
 #p3 = GPIO.PWM(22, 1)
 p1.start(0)
 
 p1.ChangeDutyCycle(50)
-
-
 p1.ChangeFrequency(1)
 time.sleep(3); 
 p1.ChangeFrequency(1*2)
