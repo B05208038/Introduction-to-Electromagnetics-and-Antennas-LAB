ece855185505a4ff979d5e236404670636cbca11
Jui-Wen, Yeh <B05208038@ntu.edu.tw>
2019-05-16 11:09:46 +0800

    infrad test, 2 lights

---
+++ Lab03/infrad_test.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as IO
+import time 
+IO.setwarnings(False)
+IO.setmode(IIO.BOARD)
+
+IO.setup(12, IO.OUT)
+IO.setup(16, IO.OUT)
+IO.setup(37, IO.IN)
+
+while 1: 
+    if (IO.input(37)==True):
+        IO.output(13, True)
+        IO.output(15, False)
+    if (IO.input(37)==False):
+        IO.output(13, False)
+        IO.output(15, True)
+IO.cleanup()
