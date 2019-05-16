35df9f34d5dcdea8c60cebb3763f331aec5be1ec
Jui-Wen, Yeh <B05208038@ntu.edu.tw>
2019-05-16 11:16:27 +0800

    infrad test, 2 lights. Fixed bug

--- Lab03/infrad_test.py
+++ Lab03/infrad_test.py
@@ -3,15 +3,15 @@ import time
 IO.setwarnings(False)
 IO.setmode(IO.BOARD)
 
-IO.setup(12, IO.OUT)
-IO.setup(16, IO.OUT)
+IO.setup(18, IO.OUT)
+IO.setup(22, IO.OUT)
 IO.setup(37, IO.IN)
 
 while 1: 
     if (IO.input(37)==True):
-        IO.output(12, True)
-        IO.output(16, False)
+        IO.output(18, True)
+        IO.output(22, False)
     if (IO.input(37)==False):
-        IO.output(12, False)
-        IO.output(16, True)
+        IO.output(18, False)
+        IO.output(22, True)
 IO.cleanup()
