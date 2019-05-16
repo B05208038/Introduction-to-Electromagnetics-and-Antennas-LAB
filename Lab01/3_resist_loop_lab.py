09b608862d6d1973df88db40820a3d090add2582
Jui-Wen, Yeh <B05208038@ntu.edu.tw>
2019-05-16 10:36:30 +0800

    blinking LED

---
+++ Lab01/3_resist_loop_lab.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import time 
+GPIO.setmode (GPIO.BOARD)
+LED_PIN1 = 16
+LED_PIN2 = 18
+LED_PIN3 = 22
+GPIO.setup(LED_PIN1, GPIO.OUT)
+GPIO.setup(LED_PIN2, GPIO.OUT)
+GPIO.setup(LED_PIN3, GPIO.OUT)
+
+try: 
+    while True: 
+        print "LED is on"
+        GPIO.output(LED_PIN1, GPIO.HIGH)
+        GPIO.output(LED_PIN2, GPIO.LOW)
+        GPIO.output(LED_PIN3, GPIO.HIGH)
+        time.sleep(1)
+
+        print "LED is off"
+        GPIO.output(LED_PIN1, GPIO.LOW)
+        GPIO.output(LED_PIN2, GPIO.HIGH)
+        GPIO.output(LED_PIN3, GPIO.LOW)
+ 
+except KeyboardInterrupt:
+    print "exception: KeyboardInterrupt"
+
+finally: 
+    GPIO.cleanup()
+
+
